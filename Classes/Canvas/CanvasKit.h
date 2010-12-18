fb074f604c13f39f432654aed9f4668559c45f52
Jason Morrissey <me@jasonmorrissey.org>
2010-12-18 16:26:02 +1100

    Re-arranged folder structure for easier importing to other projects.

---
+++ Classes/Canvas/CanvasKit.h
@@ -0,0 +1,21 @@
+//
+//  CanvasKit.h
+//  CanvasKit
+//
+//  Created by D on 18/12/10.
+//  Copyright 2010 The Design Shed. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "CanvasView.h"
+#import "CanvasViewController.h"
+#import "CanvasTileView.h"
+#import "CanvasDataSourceProtocol.h"
+#import "CanvasPageView.h"
+#import "CanvasPagePlaceholder.h"
+
+@interface CanvasKit : NSObject {
+
+}
+
+@end
